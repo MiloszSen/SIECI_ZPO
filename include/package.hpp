8c8b065d4b0921512b61daa8456b3da6ea6af9b2
Miłosz Senator <miloszsenator12@gmail.com>
2023-12-05 22:49:10 +0100

    dodanie IPackageQueue (nie wiem czy dobrze nie wiem czy w dobrym pliku ale jest jakis postep)

--- include/package.hpp
+++ include/package.hpp
@@ -4,18 +4,6 @@
 class Package{
 
 };
-class IPackageStockpile{
-public:
-    using const_iterator = std::list<Package>::const_iterator;
 
-    virtual ~IPackageStockpile() = default;
 
-    virtual void push(const Package& package) = 0;
-    virtual const_iterator cbegin() const = 0;
-    virtual const_iterator cend() const = 0;
-    virtual const_iterator begin() const = 0;
-    virtual const_iterator end() const = 0;
-    virtual std::size_t size() const = 0;
-    virtual bool empty() const = 0;
-};
 #endif //SIECI_ZPO_PACKAGE_HPP
--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -1,8 +1,20 @@
-//
-// Created by agnie on 05.12.2023.
-//
-
 #ifndef SIECI_ZPO_STORAGE_TYPES_HPP
 #define SIECI_ZPO_STORAGE_TYPES_HPP
+#include "package.hpp"
+#include <list>
 
+class IPackageStockpile{
+public:
+    using const_iterator = std::list<Package>::const_iterator;
+
+    virtual ~IPackageStockpile() = default;
+
+    virtual void push(const Package& package) = 0;
+    virtual const_iterator cbegin() const = 0;
+    virtual const_iterator cend() const = 0;
+    virtual const_iterator begin() const = 0;
+    virtual const_iterator end() const = 0;
+    virtual std::size_t size() const = 0;
+    virtual bool empty() const = 0;
+};
 #endif //SIECI_ZPO_STORAGE_TYPES_HPP
